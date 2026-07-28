b51366b708ec90e4449d2bce84a0cb9f670ff62f
Haris84 <hariss446644@gmail.com>
2026-07-28 08:32:23 +0500

    Databse file added

---
+++ Database.sql
@@ -0,0 +1,46 @@
+use student_management;
+
+INSERT INTO students
+(student_id, full_name, dob, gender, class, course, phone, email)
+VALUES
+('1001', 'Muhammad Ali', '2008-01-15', 'Male', '10th', 'Computer Science', '03001234567', 'ali@gmail.com'),
+
+('1002', 'Ahmed Raza', '2007-05-20', 'Male', '9th', 'Engineering', '03011234567', 'ahmed@gmail.com'),
+
+('1003', 'Fatima Khan', '2008-08-12', 'Female', '10th', 'Pre-Medical', '03021234567', 'fatima@gmail.com'),
+
+('1004', 'Ayesha Noor', '2007-11-02', 'Female', '9th', 'Commerce', '03031234567', 'ayesha@gmail.com'),
+
+('1005', 'Bilal Ahmed', '2008-03-25', 'Male', '10th', 'Computer Science', '03041234567', 'bilal@gmail.com'),
+
+('1006', 'Hassan Ali', '2007-07-10', 'Male', '9th', 'Engineering', '03051234567', 'hassan@gmail.com'),
+
+('1007', 'Zainab Malik', '2008-09-18', 'Female', '10th', 'Pre-Medical', '03061234567', 'zainab@gmail.com'),
+
+('1008', 'Usman Tariq', '2007-04-06', 'Male', '9th', 'Commerce', '03071234567', 'usman@gmail.com'),
+
+('1009', 'Maryam Shah', '2008-06-30', 'Female', '10th', 'Computer Science', '03081234567', 'maryam@gmail.com'),
+
+('1010', 'Hamza Khan', '2007-12-22', 'Male', '9th', 'Engineering', '03091234567', 'hamza@gmail.com'),
+
+('1011', 'Sara Ahmed', '2008-02-14', 'Female', '10th', 'Pre-Medical', '03101234567', 'sara@gmail.com'),
+
+('1012', 'Abdullah Riaz', '2007-10-09', 'Male', '9th', 'Commerce', '03111234567', 'abdullah@gmail.com'),
+
+('1013', 'Iqra Javed', '2008-05-27', 'Female', '10th', 'Computer Science', '03121234567', 'iqra@gmail.com'),
+
+('1014', 'Talha Aslam', '2007-08-15', 'Male', '9th', 'Engineering', '03131234567', 'talha@gmail.com'),
+
+('1015', 'Laiba Noor', '2008-04-11', 'Female', '10th', 'Pre-Medical', '03141234567', 'laiba@gmail.com'),
+
+('1016', 'Huzaifa Khan', '2007-01-29', 'Male', '9th', 'Commerce', '03151234567', 'huzaifa@gmail.com'),
+
+('1017', 'Alina Fatima', '2008-07-05', 'Female', '10th', 'Computer Science', '03161234567', 'alina@gmail.com'),
+
+('1018', 'Saad Ahmed', '2007-09-19', 'Male', '9th', 'Engineering', '03171234567', 'saad@gmail.com'),
+
+('1019', 'Hira Malik', '2008-11-08', 'Female', '10th', 'Pre-Medical', '03181234567', 'hira@gmail.com'),
+
+('1020', 'Umar Farooq', '2007-06-16', 'Male', '9th', 'Commerce', '03191234567', 'umar@gmail.com');
+
+select * from students
